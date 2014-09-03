23befeb5fbd8ce317120acf6d737480fcd258bc0
Juan Le Roux <juanleroux@gmail.com>
2014-09-03 14:19:00 +1000

    11

---
+++ archive/uploads/time.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+ 
+import time
+
+# Function definition is here
+def hrs( str ):
+   print str;
+   return;
+
+# Function definition is here
+def min( str ):
+   print str;
+   return;
+
+# Function definition is here
+def sec( str ):
+   print str;
+   return;
+
+hrs(time.strftime("%H"));
+min(time.strftime("%M"));
+sec(time.strftime("%S"));
